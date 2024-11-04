c5856aff9d7fb1c1aecfae2cff172b66d3410186
Andrew Sen <andrewsenth@gmail.com>
2024-11-04 08:52:37 -0600

    init discord bot

--- discord_bot.py
+++ discord_bot.py
@@ -1,1 +1,20 @@
+import os
+from dotenv import load_dotenv
+from discord.ext import commands
 
+
+def main():
+    bot = commands.Bot(command_prefix='/', description='A simple Discord bot')
+
+    load_dotenv()
+    token = os.getenv('BOT_TOKEN')
+
+    @bot.command()
+    async def hello(ctx):
+        await ctx.send("Hello!")
+
+    bot.run(token)
+
+
+if __name__ == '__main__':
+    main()
