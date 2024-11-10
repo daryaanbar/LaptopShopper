77851280b98181009ddf44c21b5fa101bdc22305
Andrew Sen <andrewsenth@gmail.com>
2024-11-09 18:50:57 -0600

    fix bot intents again

--- discord_bot.py
+++ discord_bot.py
@@ -5,9 +5,8 @@ from discord.ext import commands
 
 
 intents = discord.Intents.default()
-intent = discord.Intents.default()
-intent.members = True
-intent.message_content = True
+intents.members = True
+intents.message_content = True
 
 bot = commands.Bot(
     intents=intents,
