4e7578b06daa397d7afc3a996f14df07cb8ec438
Andrew Sen <andrewsenth@gmail.com>
2024-11-09 18:33:42 -0600

    remove main function from bot script

--- discord_bot.py
+++ discord_bot.py
@@ -4,22 +4,19 @@ import discord
 from discord.ext import commands
 
 
-def main():
-    intents = discord.Intents.default()
-    bot = commands.Bot(
-        intents=intents,
-        command_prefix='..',
-        description='A simple Discord bot')
+intents = discord.Intents.default()
+bot = commands.Bot(
+    intents=intents,
+    command_prefix='..',
+    description='A simple Discord bot')
 
-    load_dotenv()
-    token = os.getenv('BOT_TOKEN')
-
-    @bot.command()
-    async def hello(ctx):
-        await ctx.send("Hello!")
-
-    bot.run(token)
+load_dotenv()
+token = os.getenv('BOT_TOKEN')
 
 
-if __name__ == '__main__':
-    main()
+@bot.command()
+async def hello(ctx):
+    await ctx.send("Hello!")
+
+
+bot.run(token)
