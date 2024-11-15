363159411b986f5e2e7c8cf548c17186f818e43f
Andrew Sen <andrewsenth@gmail.com>
2024-11-14 20:32:55 -0600

    first try for model pipeline

--- discord_bot.py
+++ discord_bot.py
@@ -2,8 +2,39 @@ import os
 from dotenv import load_dotenv
 import discord
 from discord.ext import commands
+from transformers import pipeline
+import torch
 
-#bump
+
+MODEL_PATH = '/home/andrewsenth/.llama/checkpoints/Llama3.2-1B-Instruct'
+
+pipe = pipeline(
+    "text-generation",
+    model=MODEL_PATH,
+    torch_dtype=torch.bfloat16,
+    device_map="auto"
+)
+
+SYSTEM_PROMPT = """
+    You are a discord bot meant to give laptop recommendations based
+    on user input. You will ask for information about the user's needs
+    and use cases as necessary to give the best recommendation possible.
+    """
+
+
+def make_response(text):
+    message_structure = [
+        {"role": "system", "content": SYSTEM_PROMPT},
+        {"role": "user", "content": text}
+    ]
+
+    response_obj = pipe(
+        message_structure,
+        max_new_tokens=500
+    )
+
+    response = response_obj[0]["generated_text"][-1]["content"]
+    return response
 
 
 intents = discord.Intents.default()
@@ -20,8 +51,14 @@ token = os.getenv('BOT_TOKEN')
 
 
 @bot.command()
-async def hello(ctx):
-    await ctx.send("Hello!")
+async def ping(ctx):
+    await ctx.send("pong")
+
+
+@bot.event
+async def on_message(message):
+    if (message.content.startswith('~')):
+        await message.channel.send(make_response(message.content), reference=message)
 
 
 bot.run(token)
